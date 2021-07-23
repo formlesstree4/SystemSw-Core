d9b03988394e763d0399011d46db880d97eafd7a
Adam Sears <asears2@live.com>
2021-07-23 18:57:36 -0500

    #1 - Built the initial communication device interface. Next steps are to build abstractions using it and start breaking apart objects

---
+++ SystemSw-Core/Extron/ICommunicationDevice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SystemSw_Core.Extron
+{
+
+    /// <summary>
+    /// Defines a generic external communication device
+    /// </summary>
+    public interface ICommunicationDevice : IDisposable
+    {
+
+        /// <summary>
+        /// Gets whether or not there is an open communication channel to the device
+        /// </summary>
+        bool IsOpen { get; }
+
+        /// <summary>
+        /// Opens the connection to the device
+        /// </summary>
+        void Open();
+
+        /// <summary>
+        /// Closes the connection to the device
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Synchronously writes the given string to the device
+        /// </summary>
+        /// <param name="text">The string of text to write to the device</param>
+        void Write(string text);
+
+        /// <summary>
+        /// Reads characters from the input buffer until a <seealso cref="Environment.NewLine"/> is encountered
+        /// </summary>
+        /// <returns>The string that was read</returns>
+        string ReadLine();
+
+    }
+
+}
