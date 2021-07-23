d33f3c70bf48fd6ac33fd5434c90820f2eca29c7
Adam Sears <asears2@live.com>
2021-07-22 23:34:55 -0500

    Implemented the rest of the APIs I want to use for the moment.

--- SystemSw-Core/Extron/ExtronCommunicator.cs
+++ SystemSw-Core/Extron/ExtronCommunicator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO.Ports;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -37,6 +38,16 @@ namespace SystemSw_Core.Extron
         /// Gets the number of channels that the connected system supports
         /// </summary>
         public int Channels { get; private set; } = 0;
+
+        /// <summary>
+        /// Gets the current channel the video feed is playing on
+        /// </summary>
+        public int VideoChannel { get; private set; }
+
+        /// <summary>
+        /// Gets the current channel the audio feed is playing on
+        /// </summary>
+        public int AudioChannel { get; private set; }
 
         /// <summary>
         /// Gets the current output details for the connected system
@@ -82,6 +93,7 @@ namespace SystemSw_Core.Extron
         public string ProjectorFirmwareVersion { get; private set; }
 
 
+
         /// <summary>
         /// Creates a new instance of the <see cref="ExtronCommunicator"/> for the given serial port
         /// </summary>
@@ -89,7 +101,7 @@ namespace SystemSw_Core.Extron
         /// <param name="open">Immediately invoke <see cref="OpenConnection"/> if true</param>
         /// <param name="readTimeout">How fast the <see cref="System.IO.Ports.SerialPort"/> class will timeout a read operation</param>
         public ExtronCommunicator(
-            string serialPort, bool open, int readTimeout = 1000)
+            string serialPort, bool open = false, int readTimeout = 1000)
         {
             SerialPortAddress = serialPort;
             SerialPort = new SerialPort(serialPort)
@@ -100,9 +112,7 @@ namespace SystemSw_Core.Extron
                 DataBits = 8,
                 Handshake = Handshake.None,
                 DtrEnable = true,
-                RtsEnable = false,
-                ReadTimeout = readTimeout,
-                Encoding = System.Text.Encoding.ASCII
+                RtsEnable = false
             };
             lastReceivedResponse = "";
             history = new Stack<string>();
@@ -117,17 +127,19 @@ namespace SystemSw_Core.Extron
         /// <summary>
         /// Attempts to open the connection
         /// </summary>
+        /// <param name="doNotIdentify">If set to true, will not run Identify</param>
         /// <exception cref="System.UnauthorizedAccessException"/>
         /// <exception cref="System.ArgumentOutOfRangeException"/>
         /// <exception cref="System.ArgumentException"/>
         /// <exception cref="System.InvalidOperationException"/>
         /// <exception cref="System.IO.IOException"/>
-        public void OpenConnection()
+        public void OpenConnection(bool doNotIdentify = false)
         {
             if (SerialPort.IsOpen) return;
             SerialPort.Open();
             source = new CancellationTokenSource();
             readLoop = Task.Run(InternalReadLoop);
+            if (!doNotIdentify) Identify();
         }
 
         /// <summary>
@@ -151,77 +163,293 @@ namespace SystemSw_Core.Extron
 
 
         /// <summary>
-        /// Identifies what the connected system is.
+        /// Identifies what the connected system is and sets up initial settings.
         /// </summary>
-        /// <returns>A boolean value to </returns>
-        public async Task<bool> Identify()
+        /// <returns>A promise to indicate if identification was successful</returns>
+        public void Identify()
         {
             Write(command: "I");
+        }
 
-            var identifyResponse = await WaitForResponseAsync();
-            /*
-                The response to identifying is as follows:
+        /// <summary>
+        /// Changes the audio and video channels
+        /// </summary>
+        /// <param name="channel">The channel to change to</param>
+        /// <returns>A promise to indicate if it was successful or not</returns>
+        public void ChangeChannel(int channel)
+        {
+            if (channel < 0 || channel > Channels)
+            {
+                return;
+            }
 
-                Vx      - Video and Channel Number
-                Ax      - Audio and Channel Number
-                Tx      - Video Type (Enum)
-                Px      - Display Power status
-                Sx      - Display Mute status
-                Zx      - Switcher Mute
-                Rx      - RGB Mute
-                QSCx.xx
-                QPCx.xx
-                Mx
-             
-             */
+            Write($"{channel}!");
+        }
 
+        /// <summary>
+        /// Changes the video channel
+        /// </summary>
+        /// <param name="channel">The channel to change to</param>
+        /// <returns>A promise to indicate if it was successful or not</returns>
+        public void ChangeVideoChannel(int channel)
+        {
+            if (channel < 0 || channel > Channels)
+            {
+                return;
+            }
+
+            Write($"{channel}&");
+        }
+
+        /// <summary>
+        /// Changes the audio channel
+        /// </summary>
+        /// <param name="channel">The channel to change to</param>
+        /// <returns>A promise to indicate if it was successful or not</returns>
+        public void ChangeAudioChannel(int channel)
+        {
+            if (channel < 0 || channel > Channels)
+            {
+                return;
+            }
+
+            Write($"{channel}$");
+        }
+
+        /// <summary>
+        /// Updates the software version numbers for the Extron
+        /// </summary>
+        public void GetSoftwareVersions()
+        {
+            Write("q");
         }
 
 
 
+        /// <summary>
+        /// Sets the projector power if it is connected
+        /// </summary>
+        /// <param name="powered">true to set power on</param>
+        public void SetProjectorPower(bool powered)
+        {
+            Write(powered ? "[" : "]");
+        }
+
+        /// <summary>
+        /// Sets the projector visibility if it is connected
+        /// </summary>
+        /// <param name="visible">true to set visibility to on</param>
+        public void SetProjectorVisibility(bool visible)
+        {
+            Write(visible ? ")": "(");
+        }
+
+        /// <summary>
+        /// Set the RGB visibility state
+        /// </summary>
+        /// <param name="visible">true to set visibility to on</param>
+        public void SetRgbVisibility(bool visible)
+        {
+            Write(visible ? "b" : "B");
+        }
+
+        /// <summary>
+        /// Sets whether or not the audio line is muted or not
+        /// </summary>
+        /// <param name="muted">true to mute</param>
+        public void SetAudioState(bool muted)
+        {
+            Write(muted ? "+" : "-");
+        }
 
 
+
+        /// <summary>
+        /// Polls the serial port for incoming data
+        /// </summary>
         private void InternalReadLoop()
         {
             while(!source.IsCancellationRequested)
             {
                 try
                 {
-                    var emptyLine = SerialPort.ReadLine();
                     var dataLine = SerialPort.ReadLine();
-                    var emptyLine2 = SerialPort.ReadLine();
+                    if (!string.IsNullOrEmpty(dataLine))
+                    {
+                        HandleIncomingResponse(dataLine);
+                    }
                 }
-                catch (TimeoutException te)
+                catch (TimeoutException)
                 {
-
-                    
+                    // no worries   
+                }
+                catch (Exception)
+                {
+                    // something has gone horribly wrong [!]
                 }
             }
         }
-
 
         /// <summary>
-        /// Waits until there is a response in the buffer and returns it to the caller
+        /// Handles the incoming response message
         /// </summary>
-        /// <param name="cancellationToken">An optional <see cref="CancellationToken"/> that allows for the wait to be aborted</param>
-        /// <returns>A promise to return a string</returns>
-        private async Task<string> WaitForResponseAsync(CancellationToken cancellationToken = default)
+        /// <param name="response">The response string to interpret</param>
+        private void HandleIncomingResponse(string response)
         {
-            while(string.IsNullOrEmpty(lastReceivedResponse) && !cancellationToken.IsCancellationRequested)
+            if (string.IsNullOrWhiteSpace(response) || string.IsNullOrEmpty(response)) return;
+            history.Push($"< {response}");
+            
+            // let's handle all the permutations I care about
+            switch (response[0])
             {
-                await Task.Delay(100, cancellationToken);
+                case 'C':
+                case 'c':
+                    AudioChannel = int.Parse(response.Substring(1));
+                    VideoChannel = AudioChannel;
+                    break;
+                case 'A':
+                case 'a':
+                    // the 'AMUT' command can also fuck things up here
+                    if (response.Length > 3 && response.StartsWith("AMUT", StringComparison.OrdinalIgnoreCase))
+                    {
+                        IsAudioMuted = response.Last() == '1';
+                    }
+                    else
+                    {
+                        AudioChannel = int.Parse(response.Substring(1));
+                    }
+                    break;
+                case 'V':
+                case 'v':
+                    // Identify can also affect this command
+                    if (response.Length > 3)
+                    {
+                        HandleIdentifyString(response);
+                    }
+                    else
+                    {
+                        VideoChannel = int.Parse(response.Substring(1));
+                    }
+                    break;
+                case 'B':
+                case 'b':
+                    IsRgbMuted = response.Last() == '1';
+                    break;
+                case 'Q':
+                case 'q':
+                    if (response.StartsWith("qsc", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SwitcherFirmwareVersion = response.Substring(3);
+                    }
+                    else
+                    {
+                        ProjectorFirmwareVersion = response.Substring(3);
+                    }
+                    break;
+                case 'M':
+                case 'm':
+                    IsProjectorMuted = response.Last() == '1';
+                    break;
+                case 'P':
+                case 'p':
+                    if (response.StartsWith("PR"))
+                    {
+                        IsProjecterPowered = response.Last() == '1';
+                    }
+                    break;
             }
-            lock(lastReceivedResponse)
-            {
-                var response = lastReceivedResponse;
-                lastReceivedResponse = "";
-                return response;
-            }
+
         }
 
+        /// <summary>
+        /// Properly parses the response of the 'I' / 'i' command
+        /// </summary>
+        /// <param name="identifyResponse">The identify command</param>
+        private void HandleIdentifyString(string identifyResponse)
+        {
+            var responseArray = identifyResponse.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            /*
+                The response to identifying is as follows:
+
+                [0] Vx      - Video and Channel Number
+                [1] Ax      - Audio and Channel Number
+                [2] Tx      - Video Type (Enum)
+                [3] Px      - Projector Power status
+                [4] Sx      - Projector Display (muted) status
+                [5] Zx      - Switcher Mute
+                [6] Rx      - RGB Mute
+                [7] QSCx.xx - Switcher Firmware Version
+                [8] QPCx.xx - Switcher Projector Firmware Version
+                [9] Mx      - Maximum Channels
+             */
+            if (int.TryParse(responseArray[9].Substring(1), out var mc))
+            {
+                Channels = mc;
+            }
+            if (int.TryParse(responseArray[0].Substring(1), out var vc))
+            {
+                VideoChannel = vc;
+            }
+            if (int.TryParse(responseArray[1].Substring(1), out var ac))
+            {
+                AudioChannel = vc;
+            }
+            if (Enum.TryParse<VideoTypeEnum>(responseArray[2].Substring(1), out var vte))
+            {
+                VideoType = vte;
+            }
+            if (int.TryParse(responseArray[3].Substring(1), out var pps))
+            {
+                IsProjecterPowered = pps == 1;
+            }
+            if (int.TryParse(responseArray[4].Substring(1), out var pds))
+            {
+                IsProjectorMuted = pds == 1;
+            }
+            if (int.TryParse(responseArray[5].Substring(1), out var sm))
+            {
+                IsAudioMuted = sm == 1;
+            }
+            if (int.TryParse(responseArray[6].Substring(1), out var rgbm))
+            {
+                IsRgbMuted = rgbm == 1;
+            }
+            SwitcherFirmwareVersion = responseArray[7].Substring(3);
+            ProjectorFirmwareVersion = responseArray[8].Substring(3);
+        }
+
+        /// <summary>
+        /// Writes a command to the serial port
+        /// </summary>
+        /// <param name="command">The command to write</param>
         private void Write(string command)
         {
             SerialPort.Write(command);
+            history.Push("> {command}");
+        }
+
+        private bool IsResponseError(string response)
+        {
+            return response[0] == 'E' || response[0] == 'e';
+        }
+
+        private string GetErrorMessage(string errorCode)
+        {
+            switch (errorCode.ToUpper())
+            {
+                case "E01":
+                    return "Invalid Channel Number";
+                case "E02":
+                    return "Slave Communication Error";
+                case "E03":
+                    return "Projector is powered OFF";
+                case "E04":
+                    return "Projector Commmunication Error";
+                case "E06":
+                    return "VLB switch enabled & last input selected";
+            }
+            return $"Unknown Error Code {errorCode}";
         }
 
 
--- SystemSw-Core/Program.cs
+++ SystemSw-Core/Program.cs
@@ -1,169 +1,39 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System;
-using System.ComponentModel;
-using System.IO.Ports;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security;
+﻿using System;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SystemSw_Core
 {
     class Program
     {
 
-        static SerialPort serialPort;
-        static CancellationTokenSource cts;
-        static SemaphoreSlim ss = new SemaphoreSlim(0);
-
         static void Main(string[] args)
         {
-            cts = new CancellationTokenSource();
-            serialPort = new SerialPort
-            {
-                PortName = "COM5",
-                BaudRate = 9600,
-                Parity = Parity.None,
-                StopBits = StopBits.One,
-                DataBits = 8,
-                Handshake = Handshake.None,
-                DtrEnable = true,
-                RtsEnable = false,
-                ReadTimeout = 1000,
-                //WriteTimeout = 1000,
-                Encoding = System.Text.Encoding.ASCII
-            };
-            serialPort.Open();
-            serialPort.ProperlyInitializeNonsense(0x1A, 0xFF, 0xFF, 0x11, 0x13, 0, 768);
-            if (!serialPort.IsOpen) return;
+            var ec = new Extron.ExtronCommunicator("/dev/ttyUSB0", false);
+            Console.WriteLine("Press [ENTER] to open communication");
+            Console.ReadLine();
+            ec.OpenConnection(false);
+            Console.WriteLine("Press [ENTER] to identify");
+            Console.ReadLine();
+            ec.Identify();
 
-            Console.CancelKeyPress += (o, e) =>
-            {
-                //cts.Cancel();
-                cts.Cancel();
-            };
+            Console.WriteLine($"Channels: {ec.Channels}");
+            Console.WriteLine($"Video Channel: {ec.VideoChannel}");
+            Console.WriteLine($"Audio Channel: {ec.AudioChannel}");
 
-            serialPort.DiscardInBuffer();
-            serialPort.DiscardOutBuffer();
+            Console.WriteLine("Press [ENTER] to begin the cycle check");
+            Console.ReadLine();
 
-            // Kick off read thread
-            Task.Run(Read, cts.Token);
-
-            //Console.WriteLine("> I");
-            ////serialPort.Write(new byte[] { 0x49 }, 0, 1);
-            //serialPort.ReadExisting();
-            //serialPort.Write("q");
-
-            while(!cts.Token.IsCancellationRequested)
-            {
-                Console.Write("> ");
-                var command = Console.ReadLine()?.Trim();
-                if (string.IsNullOrWhiteSpace(command)) continue;
-                serialPort.Write(command);
-                ss.Release();
-            }
-
-            //var buffer = new byte[1];
-            //var response = serialPort.Read(buffer, 0, 1);
-            //Console.WriteLine($"response: {response}");
-            //Console.ReadLine();
-            serialPort.Close();
+            ec.ChangeChannel(1);
+            Thread.Sleep(1500);
+            ec.ChangeChannel(2);
+            Thread.Sleep(1500);
+            ec.ChangeChannel(3);
+            Thread.Sleep(1500);
+            ec.ChangeChannel(9);
 
 
         }
-
-        static void Read()
-        {
-            while (!cts.Token.IsCancellationRequested)
-            {
-                try
-                {
-                    ss.Wait();
-                    string message = serialPort.ReadLine();
-                    Console.WriteLine();
-                    Console.WriteLine($"< {message}");
-                    Console.WriteLine();
-                    Console.Write("> ");
-                }
-                catch (TimeoutException) { }
-            }
-        }
-
 
     }
-
-
-    internal static class SerialPortExtensions
-    {
-        public static void ProperlyInitializeNonsense(
-            this SerialPort port, byte eof, byte error, byte @break,
-            byte xon, byte xoff, ushort xOnLimit, ushort xOffLimit)
-        {
-            if (port == null)
-                throw new NullReferenceException();
-            if (port.BaseStream == null)
-                throw new InvalidOperationException("Cannot change X chars until after the port has been opened.");
-
-            try
-            {
-                // Get the base stream and its type which is System.IO.Ports.SerialStream
-                object baseStream = port.BaseStream;
-                Type baseStreamType = baseStream.GetType();
-
-                // Get the Win32 file handle for the port
-                SafeFileHandle portFileHandle = (SafeFileHandle)baseStreamType.GetField("_handle", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(baseStream);
-
-                // Get the value of the private DCB field (a value type)
-                FieldInfo dcbFieldInfo = baseStreamType.GetField("_dcb", BindingFlags.NonPublic | BindingFlags.Instance);
-                object dcbValue = dcbFieldInfo.GetValue(baseStream);
-
-                // The type of dcb is Microsoft.Win32.UnsafeNativeMethods.DCB which is an internal type. We can only access it through reflection.
-                Type dcbType = dcbValue.GetType();
-                dcbType.GetField("XonChar").SetValue(dcbValue, xon);
-                dcbType.GetField("XoffChar").SetValue(dcbValue, xoff);
-                dcbType.GetField("EofChar").SetValue(dcbValue, eof);
-                dcbType.GetField("ErrorChar").SetValue(dcbValue, error);
-                dcbType.GetField("XonLim").SetValue(dcbValue, xOnLimit);
-                dcbType.GetField("XoffLim").SetValue(dcbValue, xOffLimit);
-
-                //dcbType.GetField("XoffChar").SetValue(dcbValue, xoff);
-
-                // We need to call SetCommState but because dcbValue is a private type, we don't have enough
-                //  information to create a p/Invoke declaration for it. We have to do the marshalling manually.
-
-                // Create unmanaged memory to copy DCB into
-                IntPtr hGlobal = Marshal.AllocHGlobal(Marshal.SizeOf(dcbValue));
-                try
-                {
-                    // Copy their DCB value to unmanaged memory
-                    Marshal.StructureToPtr(dcbValue, hGlobal, false);
-
-                    // Call SetCommState
-                    if (!SetCommState(portFileHandle, hGlobal))
-                        throw new Win32Exception(Marshal.GetLastWin32Error());
-
-                    // Update the BaseStream.dcb field if SetCommState succeeded
-                    dcbFieldInfo.SetValue(baseStream, dcbValue);
-                }
-                finally
-                {
-                    if (hGlobal != IntPtr.Zero)
-                        Marshal.FreeHGlobal(hGlobal);
-                }
-            }
-            catch (SecurityException) { throw; }
-            catch (OutOfMemoryException) { throw; }
-            catch (Win32Exception) { throw; }
-            catch (Exception ex)
-            {
-                throw new ApplicationException("SetXonXoffChars has failed due to incorrect assumptions about System.IO.Ports.SerialStream which is an internal type.", ex);
-            }
-        }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool SetCommState(SafeFileHandle hFile, IntPtr lpDCB);
-    }
-
 
 }
