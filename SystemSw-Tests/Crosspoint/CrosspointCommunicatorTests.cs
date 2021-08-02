f10af0c6470e51ae548b1f35d7e0c0e3231b24c1
Adam Sears <asears2@live.com>
2021-08-01 22:34:07 -0500

    #12 - Got the class actually setup...

--- SystemSw-Tests/Crosspoint/CrosspointCommunicatorTests.cs
+++ SystemSw-Tests/Crosspoint/CrosspointCommunicatorTests.cs
@@ -1,6 +1,30 @@
-﻿namespace SystemSw.Sharp.Tests.Crosspoint
+﻿using Crosspoint.Communicator;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace SystemSw.Sharp.Tests.Crosspoint
 {
     public class CrosspointCommunicatorTests
     {
+
+        private readonly ILogger<ExtronCrosspointCommunicator> logger;
+
+
+        public CrosspointCommunicatorTests()
+        {
+            logger = new NullLogger<ExtronCrosspointCommunicator>();
+        }
+
+
+        private static IConfiguration GetConfiguration(bool autoload)
+        {
+            var filename = autoload ? "appsettings.json" : "appsettings.noautoload.json";
+            return new ConfigurationBuilder()
+                .AddJsonFile(filename, false, true)
+                .Build();
+        }
+
     }
+
 }
