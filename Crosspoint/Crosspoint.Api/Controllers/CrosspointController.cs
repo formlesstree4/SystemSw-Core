696b7aa5941fd384a3b1fc56a184cd4abdd2e00e
Adam Sears <asears2@live.com>
2021-08-07 00:11:32 -0500

    Getting configs setup

--- Crosspoint/Crosspoint.Api/Controllers/CrosspointController.cs
+++ Crosspoint/Crosspoint.Api/Controllers/CrosspointController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Crosspoint.Api.Controllers
 {
@@ -20,16 +17,9 @@ namespace Crosspoint.Api.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Models.CrosspointMapping> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return null;
         }
     }
 }
